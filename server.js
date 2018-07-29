55d94c3576745f216477510d3ba05b5874129df6
s5mathe <Sunil.Mathew0@walmart.com>
2018-07-29 07:07:08 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+var app = express();
+
+const hbs = require('hbs'); //Handle Bars code
+const fs = require('fs');
+
+app.set('view engine','hbs');
+
+app.use((req,res,next)=> {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log (log);
+    fs.appendFile('server.log',log +'\n',err=>{
+        console.log('Unable to append File');   
+    })
+    next();
+})
+
+// app.use((req,res) => {
+//     res.render('maintenance.hbs',{
+//         pageTitle:'Down For Maintenance',
+//         currentYear: new Date().getFullYear()
+//     })
+// })
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(req,res)=> {
+    // res.send('Hello Express');
+    res.render('welcome.hbs',{
+        pageTitle: 'Welcome Page',
+        welcomeMsg: 'Welcome Sunil',
+        currentYear:new Date().getFullYear()
+    })
+});
+
+app.get('/about',(req,res)=> {
+    res.render('about.hbs',{
+        pageTitle: 'Sunils Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad',(req,res)=> {
+    res.send({
+        error: 'Error Message',
+        errdetails:[
+            'Error due to socket connection',
+            'Bad parameter'
+        ]
+    })
+})
+app.listen(3100,()=>{
+    console.log('Server started on port 3100');
+});
+
+
