17da5af2f0dc49756098bda94930b6947106a75c
s5mathe <Sunil.Mathew0@walmart.com>
2018-08-06 05:27:53 +0530

    Modified port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 const hbs = require('hbs'); //Handle Bars code
 const fs = require('fs');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine','hbs');
